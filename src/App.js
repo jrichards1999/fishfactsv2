0298a3a7d5c71f2f062d16ebe628aa9c9e415b0f
jrichards1999 <jrichards050399@gmail.com>
2022-09-03 17:58:01 -0400

    Got things ported over and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const [searchBoxText, setSearchBoxText] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [imageSrc, setImageSrc] = useState("");
+
+  function handleClick() {
+    const Url =
+      "https://en.wikipedia.org/api/rest_v1/page/summary/" + searchBoxText;
+    fetch(Url, { method: "GET" })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        console.log(json);
+        try {
+          setTitle(json.title);
+          setContent(json.extract);
+          setImageSrc(json.originalimage.source);
+          console.log(json.title);
+          console.log(json.extract);
+          console.log(json.thumbnail.source);
+        } catch {
+          setTitle("No Exact Match Found");
+          setContent("");
+          setImageSrc("");
+        }
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer className="jumbotron">
+      <ContentContainer>
+        <h1>Wikipedia API Demonstration</h1>
+        <p>To start, type a word or phrase and click Search.</p>
+        <SearchContainer>
+          <StyledInput
+            onChange={(event) => setSearchBoxText(event.target.value)}
+            onKeyDown={(ev) => {
+              if (ev.key === "Enter") {
+                ev.preventDefault();
+                handleClick();
+              }
+            }}
+          ></StyledInput>
+          <StyledLink onClick={handleClick} className="btn btn-primary">
+            Search
+          </StyledLink>
+        </SearchContainer>
+        <ContentDiv>
+          <Title>{title}</Title>
+          <Image src={imageSrc} alt="" />
+          <ContentParagraph>{content}</ContentParagraph>
+        </ContentDiv>
+      </ContentContainer>
+    </PageContainer>
   );
 }
+
+const PageContainer = styled.div`
+  margin: 25px;
+`;
+
+const ContentContainer = styled.div`
+  padding: 25px;
+  border-radius: 25px;
+  background-color: #add8e6;
+`;
+
+const Image = styled.img`
+  max-height: 500px;
+  max-width: 500px;
+`;
+
+const Title = styled.h1`
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+const ContentParagraph = styled.p`
+  margin-top: 15px;
+  padding-left: 200px;
+  padding-right: 200px;
+  background-color: darkblue;
+  color: white;
+  border-radius: 25px;
+`;
+
+const SearchContainer = styled.div``;
+
+const StyledInput = styled.input`
+  margin-right: 5px;
+  height: 40px;
+`;
+
+const StyledLink = styled.button`
+  margin-top: -5px;
+`;
+
+const ContentDiv = styled.div`
+  align-items: center;
+  text-align: center;
+`;
 
 export default App;
