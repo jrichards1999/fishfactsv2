c0c25f574a5c7993f42e5dff7ea6f17a668e85a8
jrichards1999 <jrichards050399@gmail.com>
2022-10-29 13:55:15 -0400

    Moved styled components to their own file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import {
+  PageContainer,
+  ContentContainer,
+  Image,
+  Title,
+  ContentParagraph,
+  StyledButton,
+  ContentDiv,
+} from "./App.styles";
 import { getRandomFish, getWikiInfo } from "./Api/wikiApi";
 
 function App() {
@@ -35,68 +44,5 @@ function App() {
     </PageContainer>
   );
 }
-
-const PageContainer = styled.div`
-  margin: 25px;
-`;
-
-const ContentContainer = styled.div`
-  padding: 25px;
-  border-radius: 25px;
-  background-color: #add8e6;
-  box-shadow: 8px 5px 5px gray;
-`;
-
-const Image = styled.img`
-  max-height: 500px;
-  max-width: 500px;
-  box-shadow: 8px 5px 5px #23395d;
-`;
-
-const Title = styled.h1`
-  margin-top: 15px;
-  margin-bottom: 15px;
-`;
-
-// TODO - Media query stuff currently not working
-// export const device = {
-//   big: `(min-width: 1024)`,
-//   small: `(min-width: 768)`,
-// };
-
-const ContentParagraph = styled.p`
-  margin-top: 30px;
-  background-color: #23395d;
-  color: white;
-  border-radius: 25px;
-  padding: 20px 200px;
-`;
-
-const StyledButton = styled.button`
-  margin-top: -5px;
-  border-radius: 25px;
-  background-color: #23395d;
-  box-shadow: 8px 5px 5px gray;
-
-  :hover {
-    background-color: #33435e;
-  }
-
-  :focus {
-    background-color: #23395d;
-    box-shadow: 8px 5px 5px gray;
-  }
-`;
-
-const ContentDiv = styled.div`
-  margin-top: 15px;
-  padding-top: 1px;
-  align-items: center;
-  text-align: center;
-  background-color: #34aed1;
-
-  box-shadow: 8px 5px 5px gray;
-  border-radius: 25px;
-`;
 
 export default App;
---
+++ src/App.styles.js
@@ -0,0 +1,62 @@
+export const PageContainer = styled.div`
+  margin: 25px;
+`;
+
+export const ContentContainer = styled.div`
+  padding: 25px;
+  border-radius: 25px;
+  background-color: #add8e6;
+  box-shadow: 8px 5px 5px gray;
+`;
+
+export const Image = styled.img`
+  max-height: 500px;
+  max-width: 500px;
+  box-shadow: 8px 5px 5px #23395d;
+`;
+
+export const Title = styled.h1`
+  margin-top: 15px;
+  margin-bottom: 15px;
+`;
+
+// TODO - Media query stuff currently not working
+// export const device = {
+//   big: `(min-width: 1024)`,
+//   small: `(min-width: 768)`,
+// };
+
+export const ContentParagraph = styled.p`
+  margin-top: 30px;
+  background-color: #23395d;
+  color: white;
+  border-radius: 25px;
+  padding: 20px 200px;
+`;
+
+export const StyledButton = styled.button`
+  margin-top: -5px;
+  border-radius: 25px;
+  background-color: #23395d;
+  box-shadow: 8px 5px 5px gray;
+
+  :hover {
+    background-color: #33435e;
+  }
+
+  :focus {
+    background-color: #23395d;
+    box-shadow: 8px 5px 5px gray;
+  }
+`;
+
+export const ContentDiv = styled.div`
+  margin-top: 15px;
+  padding-top: 1px;
+  align-items: center;
+  text-align: center;
+  background-color: #34aed1;
+
+  box-shadow: 8px 5px 5px gray;
+  border-radius: 25px;
+`;
